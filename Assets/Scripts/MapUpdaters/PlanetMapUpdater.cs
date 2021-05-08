72fd0dc5f7fad02fa7792669eaedb3ca382f69af
Shawn Cook <sfncook@gmail.com>
2021-05-08 09:27:32 -0700

    load planet obj

--- Assets/Scripts/MapUpdaters/PlanetMapUpdater.cs
+++ Assets/Scripts/MapUpdaters/PlanetMapUpdater.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class PlanetMapUpdater : MonoBehaviour
 {
+    private MainGameState gameState;
+    private Planet planet;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        gameState = MainGameState.gameState;
+        planet = gameState.getPlanetByName(gameObject.name);
     }
 }
