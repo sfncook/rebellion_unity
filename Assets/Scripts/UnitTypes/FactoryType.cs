b27163f1f9a04e5363fe4d7c970728896b1f16e9
Shawn Cook <sfncook@gmail.com>
2021-05-08 19:36:14 -0700

    populate factory types

--- Assets/Scripts/UnitTypes/FactoryType.cs
+++ Assets/Scripts/UnitTypes/FactoryType.cs
@@ -1,10 +1,19 @@
-﻿public class FactoryType : AbstractType
-{
-    public static FactoryType ctorYard = new FactoryType("Construction Yard");
-    public static FactoryType shipYard = new FactoryType("Ship Yard");
-    public static FactoryType trainingFac = new FactoryType("Training Facility");
+﻿using System.Collections.Generic;
 
-    public FactoryType(string name) : base(name)
+public class FactoryType : AbstractType
+{
+    private static AbstractType[] ctorYardBuildTypes = {FactoryType.ctorYard, FactoryType.shipYard, FactoryType.trainingFac};
+    private static AbstractType[] shipYardBuildTypes = {ShipType.Bireme, ShipType.Trireme, ShipType.Quadreme, ShipType.Quadreme};
+    private static AbstractType[] trainingFacBuildTypes = { };
+
+    public static FactoryType ctorYard = new FactoryType("Construction Yard", new List<AbstractType>(ctorYardBuildTypes));
+    public static FactoryType shipYard = new FactoryType("Ship Yard", new List<AbstractType>(shipYardBuildTypes));
+    public static FactoryType trainingFac = new FactoryType("Training Facility", new List<AbstractType>(trainingFacBuildTypes));
+
+    public readonly List<AbstractType> typesAvailableToBuild = new List<AbstractType>();
+
+    public FactoryType(string name, List<AbstractType> typesAvailableToBuild) : base(name)
     {
+        typesAvailableToBuild = new List<AbstractType>(typesAvailableToBuild);
     }
 }
