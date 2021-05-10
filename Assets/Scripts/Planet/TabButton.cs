fde88c7001b699e117559868bcd8c243033b3e1f
Shawn Cook <sfncook@gmail.com>
2021-05-09 17:34:40 -0700

    Working on tabs

--- Assets/Scripts/Planet/TabButton.cs
+++ Assets/Scripts/Planet/TabButton.cs
@@ -1,17 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TabButton : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public PlanetDialog planetDialog;
+
     void Start()
     {
-        
+        //energySquare.GetComponent<SpriteRenderer>().color = Color.blue;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
+    {
+        //SceneManager.LoadScene("Sector");
+    }
+
+    private void OnGUI()
     {
         
     }
