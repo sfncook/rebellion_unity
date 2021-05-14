64b506db36aac37d8677f310293205d57f97d19a
Shawn Cook <sfncook@gmail.com>
2021-05-13 21:49:31 -0700

    parallax works

--- Assets/Scripts/Planet Detail Dialog 02/ParallaxScroll.cs
+++ Assets/Scripts/Planet Detail Dialog 02/ParallaxScroll.cs
@@ -7,6 +7,7 @@ public class ParallaxScroll : MonoBehaviour
 
     public void onScroll(Vector2 vector2)
     {
+        //Debug.Log(vector2+" bg:"+ backgroundImg.transform.position+" pl:"+ planetImg.transform.position);
         backgroundImg.transform.position = new Vector3(
             backgroundImg.transform.position.x,
             (-6.0f * vector2.y * 0.08f),
