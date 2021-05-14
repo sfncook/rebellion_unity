b48c5a81c281bf4c086ac9a27f092a228dd997b3
Shawn Cook <sfncook@gmail.com>
2021-05-13 21:41:28 -0700

    more scroll work

--- Assets/Scripts/Planet Detail Dialog 02/ParallaxScroll.cs
+++ Assets/Scripts/Planet Detail Dialog 02/ParallaxScroll.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 public class ParallaxScroll : MonoBehaviour
 {
-    public GameObject background;
+    public GameObject backgroundImg;
+    public GameObject planetImg;
 
     public void onScroll(Vector2 vector2)
     {
-        background.transform.position = new Vector3(
-            background.transform.position.x,
-            (-6.0f * vector2.y * 0.1f),
-            background.transform.position.z
+        backgroundImg.transform.position = new Vector3(
+            backgroundImg.transform.position.x,
+            (-6.0f * vector2.y * 0.08f),
+            backgroundImg.transform.position.z
+            );
+        planetImg.transform.position = new Vector3(
+            planetImg.transform.position.x,
+            (-2.0f * vector2.y) - 2.0f,
+            planetImg.transform.position.z
             );
     }
 }
