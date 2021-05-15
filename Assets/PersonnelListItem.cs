7a39ad2e249ac2682b37451afb2a457aa6b40803
Shawn Cook <sfncook@gmail.com>
2021-05-15 12:50:27 -0700

    personnel populating correctly on detail page

--- Assets/PersonnelListItem.cs
+++ Assets/PersonnelListItem.cs
@@ -6,17 +6,17 @@ public class PersonnelListItem : MonoBehaviour
 
     public void setPersonnel(Personnel personnel)
     {
-        //personnelImg.sprite = Resources.Load<Sprite>("Images/Personnel/" + personnel.type.name);
+        personnelImg.sprite = Resources.Load<Sprite>("Images/Personnel/" + personnel.type.name);
 
-        //if (personnel.team.Equals(Team.TeamA))
-        //{
-        //    personnelImg.transform.localScale = new Vector3(1, 1, 1);
-        //    personnelImg.color = Color.green;
-        //}
-        //else
-        //{
-        //    personnelImg.transform.localScale = new Vector3(-1, 1, 1);
-        //    personnelImg.color = Color.red;
-        //}
+        if (personnel.team.Equals(Team.TeamA))
+        {
+            personnelImg.transform.localScale = new Vector3(50, 50, 1);
+            personnelImg.color = Color.green;
+        }
+        else
+        {
+            personnelImg.transform.localScale = new Vector3(-50, 50, 1);
+            personnelImg.color = Color.red;
+        }
     }
 }
