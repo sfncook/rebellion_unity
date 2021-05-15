fab6736f6795ce653e6b44d9290b07162d8b816b
Shawn Cook <sfncook@gmail.com>
2021-05-15 11:54:33 -0700

    populate ships

--- Assets/Scripts/Planet Detail Dialog 02/PlanetDetail.cs
+++ Assets/Scripts/Planet Detail Dialog 02/PlanetDetail.cs
@@ -6,6 +6,9 @@ public class PlanetDetail : MonoBehaviour
     public TextMeshProUGUI planetNameLabel;
     public SpriteRenderer planetImg;
     public SpriteRenderer planetShieldImg;
+    public GameObject shipListItemPrefab;
+    public Transform shipsTeamAPanel;
+    public Transform shipsTeamBPanel;
 
 
     private MainGameState gameState;
@@ -15,8 +18,39 @@ public class PlanetDetail : MonoBehaviour
     {
         gameState = MainGameState.gameState;
         planet = gameState.planetForDetail;
-        planetImg.sprite = Resources.Load<Sprite>("Images/Planets/"+planet.name);
+        planetImg.sprite = Resources.Load<Sprite>("Images/Planets/" + planet.name);
         planetNameLabel.text = planet.name;
         planetShieldImg.enabled = false;
+        updateGrid();
+    }
+
+    private void updateGrid()
+    {
+        clearAll();
+
+        GameObject newObj;
+
+        // Ships
+        planet.shipsInOrbit.Sort((a, b) => a.type.name.CompareTo(b.type.name));
+        foreach (Ship ship in planet.shipsInOrbit)
+        {
+            Transform panelTransform = ship.team.Equals(Team.TeamA) ? shipsTeamAPanel : shipsTeamBPanel;
+            newObj = (GameObject)Instantiate(shipListItemPrefab, panelTransform);
+            ShipListItem2 shipListItem = newObj.GetComponent<ShipListItem2>();
+            shipListItem.setShip(ship);
+        }
+    }
+
+    private void clearAll()
+    {
+        foreach (Transform child in shipsTeamAPanel)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+
+        foreach (Transform child in shipsTeamBPanel)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/Planet Detail Dialog 02/ShipListItem2.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ShipListItem2 : MonoBehaviour
+{
+    public SpriteRenderer shipImg;
+    public SpriteRenderer hasPersonnelImg;
+
+    private Ship ship;
+
+    public void setShip(Ship ship)
+    {
+        this.ship = ship;
+        //shipImg.sprite = Resources.Load<Sprite>("Images/Ships/" + ship.type.name);
+        hasPersonnelImg.enabled = false;
+
+        if (ship.team.Equals(Team.TeamA))
+        {
+            shipImg.transform.localScale = new Vector3(100, 100, 1);
+            shipImg.color = Color.green;
+        }
+        else
+        {
+            shipImg.transform.localScale = new Vector3(-100, 100, 1);
+            shipImg.color = Color.red;
+        }
+    }
+}
