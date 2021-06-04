44ed34363fd036b922163bc759c5cbfb34bf8b70
Shawn Cook <sfncook@gmail.com>
2021-06-03 21:24:51 -0700

    instantiate all 5 planets

--- Assets/Scripts/GameState/Planet.cs
+++ Assets/Scripts/GameState/Planet.cs
@@ -4,6 +4,8 @@ public class Planet
 {
     // Immutable attributes:
     public readonly string name;
+    public readonly float sectorX;
+    public readonly float sectorY;
 
     // Randomized-then-immutable attributes:
     public readonly int energyCapacity;
@@ -24,10 +26,17 @@ public class Planet
     public readonly List<Defense> defenses = new List<Defense>();
     public readonly List<Defense> defensesInTransit = new List<Defense>();
 
-    public Planet(string name, int energyCapacity, float loyalty)
+    public Planet(string name, int energyCapacity, float loyalty) : this(name, energyCapacity, loyalty, 0f, 0f)
+    {
+        
+    }
+
+    public Planet(string name, int energyCapacity, float loyalty, float sectorX, float sectorY)
     {
         this.name = name;
         this.energyCapacity = energyCapacity;
         this.loyalty = loyalty;
+        this.sectorX = sectorX;
+        this.sectorY = sectorY;
     }
 }
--- Assets/Scripts/Sector Map 2/SectorMap2.cs
+++ Assets/Scripts/Sector Map 2/SectorMap2.cs
@@ -7,10 +7,26 @@ public class SectorMap2 : MonoBehaviour
 
     void Start()
     {
+        Planet ater = new Planet("Ater", Random.Range(1, 10), Random.Range(0.0f, 0.999f), 0.2f, 0.8f);
+        Planet nageron = new Planet("Nageron", Random.Range(1, 10), Random.Range(0.0f, 0.999f), 0.8f, 0.77f);
+        Planet ucholla = new Planet("Ucholla", Random.Range(1, 10), Random.Range(0.0f, 0.999f), 0.6f, 0.45f);
+        Planet obiemia = new Planet("Obiemia", Random.Range(1, 10), Random.Range(0.0f, 0.999f), 0.3f, 0.25f);
+        Planet ibos = new Planet("Ibos", Random.Range(1, 10), Random.Range(0.0f, 0.999f), 0.75f, 0.19f);
+
+        instantiatePlanet(ater);
+        instantiatePlanet(nageron);
+        instantiatePlanet(ucholla);
+        instantiatePlanet(obiemia);
+        instantiatePlanet(ibos);
+    }
+
+    private void instantiatePlanet(Planet planet)
+    {
         GameObject newPlanetObj = (GameObject)Instantiate(planetPrefab, panelForPlanets);
         RectTransform planetRectTrans = newPlanetObj.GetComponent<RectTransform>();
-        planetRectTrans.anchorMin = new Vector2(0.75f, 0.75f);
-        planetRectTrans.anchorMax = new Vector2(0.75f, 0.75f);
+        planetRectTrans.anchorMin = new Vector2(planet.sectorX, planet.sectorY);
+        planetRectTrans.anchorMax = new Vector2(planet.sectorX, planet.sectorY);
         planetRectTrans.anchoredPosition = new Vector2(0f, 0f);
+        planetRectTrans.localScale = new Vector2(2f, 2f);
     }
 }
