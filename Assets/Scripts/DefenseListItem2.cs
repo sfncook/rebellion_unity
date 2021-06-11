4ccdca65bd5f6f95fcc47f65b2308cb43b701ba6
Shawn Cook <sfncook@gmail.com>
2021-06-11 16:48:45 -0700

    defense and factory prefabs

---
+++ Assets/Scripts/DefenseListItem2.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DefenseListItem2 : MonoBehaviour
+{
+    public Image defenseImg;
+    public Image backgroundImg;
+    public ValueBars healthBars;
+
+    private Defense defense;
+
+    public void setDefense(Defense defense, Team team)
+    {
+        this.defense = defense;
+
+        defenseImg.sprite = Resources.Load<Sprite>("Images/Defenses/" + defense.type.name);
+        if (team.Equals(Team.TeamA))
+        {
+            backgroundImg.color = Color.green;
+        }
+        else if (team.Equals(Team.TeamB))
+        {
+            backgroundImg.color = Color.red;
+        }
+    }
+}
---
+++ Assets/Scripts/FactoryListItem2.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FactoryListItem2 : MonoBehaviour
+{
+    public Image factoryImg;
+    public Image backgroundImg;
+
+    private Factory factory;
+
+    public void setDefense(Factory factory, Team team)
+    {
+        this.factory = factory;
+
+        factoryImg.sprite = Resources.Load<Sprite>("Images/Factories/" + factory.type.name);
+        if (team.Equals(Team.TeamA))
+        {
+            backgroundImg.color = Color.green;
+        }
+        else if (team.Equals(Team.TeamB))
+        {
+            backgroundImg.color = Color.red;
+        }
+    }
+}
