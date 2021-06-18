8403c3027bb51225335aa26f4976272566c91179
Shawn Cook <sfncook@gmail.com>
2021-06-17 17:38:16 -0700

    clean up remove initializeGameState

--- Assets/Scripts/GameState/MainGameState.cs
+++ Assets/Scripts/GameState/MainGameState.cs
@@ -70,7 +70,7 @@ public class MainGameState : MonoBehaviour
         if(gameState == null) {
             DontDestroyOnLoad(gameObject);
             gameState = this;
-            //initializeGameState();
+
             allShipsUpdater.init();
             allPersonnelUpdater.init();
             allDefenseUpdater.init();
@@ -200,116 +200,6 @@ public class MainGameState : MonoBehaviour
     }
 
 
-    public void initializeGameState()
-    {
-        Planet ater = new Planet("Ater", Random.Range(1, 10), Random.Range(0.0f, 0.999f));
-        Planet nageron = new Planet("Nageron", Random.Range(1, 10), Random.Range(0.0f, 0.999f));
-        Planet ucholla = new Planet("Ucholla", Random.Range(1, 10), Random.Range(0.0f, 0.999f));
-        Planet obiemia = new Planet("Obiemia", Random.Range(1, 10), Random.Range(0.0f, 0.999f));
-        Planet ibos = new Planet("Ibos", Random.Range(1, 10), Random.Range(0.0f, 0.999f));
-        planets.Add(ater);
-        planets.Add(nageron);
-        planets.Add(ucholla);
-        planets.Add(obiemia);
-        planets.Add(ibos);
-
-        // Pick Team HQ Planets
-        List<Planet> planetsCopy = planets.GetRange(0, planets.Count);
-        int teamAHq = Random.Range(0, planetsCopy.Count);
-        Planet planetTeamA = planetsCopy[teamAHq];
-        planetsCopy.Remove(planetTeamA);
-        int teamBHq = Random.Range(0, planetsCopy.Count);
-        Planet planetTeamB = planetsCopy[teamBHq];
-
-        planetTeamA.isHq = true;
-        planetTeamB.isHq = true;
-
-        planetTeamA.loyalty = 0.1f;
-        planetTeamB.loyalty = 0.9f;
-
-        Ship initShipA = new Ship(ShipType.Bireme, Team.TeamA);
-        Ship initShipB = new Ship(ShipType.Bireme, Team.TeamB);
-
-        planetTeamA.shipsInOrbit.Add(initShipA);
-        planetTeamB.shipsInOrbit.Add(initShipB);
-
-        planetTeamA.factories.Add(new Factory(FactoryType.ctorYard));
-        planetTeamB.factories.Add(new Factory(FactoryType.ctorYard));
-
-
-        // Randomly add ships
-        var shipTypes = new ShipType[] {
-            ShipType.Bireme,
-            ShipType.Trireme,
-            ShipType.Quadreme,
-            ShipType.Quintreme
-        };
-        foreach (var planet in planets)
-        {
-            for(var i=0; i<Random.Range(0,3); i++)
-            {
-                Team team = (Random.Range(0.0f, 1.0f) >= 0.5f) ? Team.TeamA : Team.TeamB;
-                int typeIndex = Random.Range(0, shipTypes.Length);
-                Ship randShip = new Ship(shipTypes[typeIndex], team);
-                planet.shipsInOrbit.Add(randShip);
-            }
-        }
-
-        // Randomly add personnel
-        var personnelTypes = new PersonnelType[] {
-            PersonnelType.Diplomat,
-            PersonnelType.Soldiers,
-            PersonnelType.Spy
-        };
-        foreach (var planet in planets)
-        {
-            for (var i = 0; i < Random.Range(0, 4); i++)
-            {
-                Team team = (Random.Range(0.0f, 1.0f) >= 0.5f) ? Team.TeamA : Team.TeamB;
-                //int typeIndex = Random.Range(0, personnelTypes.Length);
-                //Personnel personnel = new Personnel(personnelTypes[typeIndex], team);
-                Personnel personnel = new Personnel(PersonnelType.Soldiers, team);
-                planet.personnelsOnSurface.Add(personnel);
-            }
-        }
-
-        // Randomly add factories
-        var factoryTypes = new FactoryType[] {
-            FactoryType.ctorYard,
-            FactoryType.shipYard,
-            FactoryType.trainingFac
-        };
-        foreach (var planet in planets)
-        {
-            int many = Mathf.Min(Random.Range(0, 3), (planet.energyCapacity - planet.factories.Count - planet.defenses.Count));
-            for (var i = 0; i < many; i++)
-            {
-                int typeIndex = Random.Range(0, factoryTypes.Length);
-                Factory factory = new Factory(factoryTypes[typeIndex]);
-                planet.factories.Add(factory);
-            }
-        }
-
-        // Randomly add defenses
-        var defenseTypes = new DefenseType[] {
-            DefenseType.orbitalBattery,
-            DefenseType.planetaryShield
-        };
-        foreach (var planet in planets)
-        {
-            int many = Mathf.Min(Random.Range(0, 3), (planet.energyCapacity - planet.factories.Count - planet.defenses.Count));
-            for (var i = 0; i < many; i++)
-            {
-                int typeIndex = Random.Range(0, defenseTypes.Length);
-                Defense defense = new Defense(defenseTypes[typeIndex]);
-                bool hasOrbitalShield = planet.defenses.Exists(defense => defense.type.Equals(DefenseType.planetaryShield));
-                if(defense.type.Equals(DefenseType.orbitalBattery) || !hasOrbitalShield)
-                {
-                    planet.defenses.Add(defense);
-                }
-            }
-        }
-    }// initializeGameState
 
     public void loadGameFromFiles()
     {
