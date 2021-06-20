988f866d854be4d840968606ba3527e5aac4e932
Shawn Cook <sfncook@gmail.com>
2021-06-20 10:35:38 -0700

    Scene load

--- Assets/Scripts/StartGameButton.cs
+++ Assets/Scripts/StartGameButton.cs
@@ -5,7 +5,7 @@ public class StartGameButton : MonoBehaviour
 {
     public void onClick()
     {
-        MainGameState.gameState.loadGameFromFiles();
-        SceneManager.LoadScene("Galaxy Map");
+        //MainGameState.gameState.loadGameFromFiles();
+        SceneManager.LoadScene("Introduction Scene");
     }
 }
